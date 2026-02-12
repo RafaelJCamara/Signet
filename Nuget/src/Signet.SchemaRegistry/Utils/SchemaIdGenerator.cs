ca49efb084ede7d9d24a2a7680f0f7ed999cb5af
RafaelJCamara <rafaelcamarac@gmail.com>
2026-02-12 22:34:07 +0100

    WIP

---
+++ Nuget/src/Signet.SchemaRegistry/Utils/SchemaIdGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace Signet.SchemaRegistry.Utils
+{
+    public static class SchemaIdGenerator
+    {
+        public static string GenerateOnPublishSchemaId(string exchange, string routingKey)
+        {
+            return $"{exchange.ToLowerInvariant()}-{routingKey.ToLowerInvariant()}";
+        }
+    }
+}
